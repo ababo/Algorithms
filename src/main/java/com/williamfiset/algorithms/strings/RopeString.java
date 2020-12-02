123f3963a500e8047d1788fc38e61918ad18e231
Kir-Push <X0jHSCB7Fv6sOtS9>
2020-12-02 21:26:21 +0200

    Rope implementation remarks fix

---
+++ src/main/java/com/williamfiset/algorithms/strings/RopeString.java
@@ -0,0 +1,274 @@
+/**
+ * Rope data structure implementation. Rope data structure is a tree data structure (binary tree)
+ * which is used to store or manipulate large strings in a more efficient manner.
+ *
+ * @author kir-push
+ */
+package com.williamfiset.algorithms.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RopeString {
+  Node root;
+
+  // construct rope from string value with only one leaf node
+  public RopeString(String value) {
+    this.root = new Node(value);
+  }
+
+  // construct rope from node. Node will be root of rope
+  public RopeString(Node node) {
+    this.root = node;
+  }
+
+  // add value to the end
+  public void append(String value) {
+    root = concatenate(this, new RopeString(value)).root;
+  }
+
+  // split rope into two by index
+  public Pair<RopeString> split(int index) {
+    Node left = new Node();
+    Node right = new Node();
+    createBranches(left, right, root, index);
+    cleanUp(left);
+    cleanUp(right);
+    return new Pair<>(new RopeString(left), new RopeString(right));
+  }
+
+  // insert string in specific position
+  public void insert(String value, int position) {
+    Pair<RopeString> ropePair = split(position);
+    RopeString firstConcat = concatenate(ropePair.left, new RopeString(value));
+    RopeString concatenate = concatenate(firstConcat, ropePair.right);
+    this.root = concatenate.root;
+  }
+
+  // remove specific size of chars, started from position
+  public void delete(int position, int size) {
+    if (size < 0 || position < 0 || position + size > root.weight) {
+      throw new IndexOutOfBoundsException();
+    }
+    Pair<RopeString> ropePair = split(position);
+    Pair<RopeString> resultEnding = ropePair.right.split(size);
+    RopeString concatenate = concatenate(ropePair.left, resultEnding.right);
+    this.root = concatenate.root;
+  }
+
+  // transform value to java string.
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    createString(root, builder);
+    return builder.toString();
+  }
+
+  // rebalance tree to optimal condition
+  public void rebalance() {
+    List<Node> nodes = new ArrayList<>();
+    collectAll(root, nodes);
+    root = combine(nodes, 0, nodes.size());
+  }
+
+  // create two branches from left and right node
+  // if current node has no child - it's leaf
+  private void createBranches(Node left, Node right, Node current, int index) {
+    if (current.isLeaf()) {
+      splitLeaf(left, right, current, index);
+    } else {
+      splitNotLeaf(left, right, current, index);
+    }
+  }
+
+  // recursively goes by rope to index, and create two new ropes in process.
+  // Node left and Node right is just two new Node which will be result of split.
+  private void splitNotLeaf(Node left, Node right, Node current, int index) {
+    left = createNodeIfNull(left);
+    right = createNodeIfNull(right);
+    current.left = createNodeIfNull(current.left);
+    current.right = createNodeIfNull(current.right);
+    left.weight = index;
+    right.weight = current.weight - index;
+
+    if (index < current.left.weight) {
+      right.right = current.right;
+      right.left = new Node();
+      createBranches(left, right.left, current.left, index);
+      right.depth = calculateDepth(right) + 1;
+    } else {
+      left.left = current.left;
+      left.right = new Node();
+      createBranches(left.right, right, current.right, index - current.left.weight);
+      left.depth = calculateDepth(left) + 1;
+    }
+  }
+
+  private Node createNodeIfNull(Node node) {
+    if (node == null) {
+      return new Node();
+    }
+    return node;
+  }
+
+  // if current node leaf, split string, and set left part to left node, and right part to right
+  // node.
+  private void splitLeaf(Node left, Node right, Node current, int index) {
+    String splitValue = current.value;
+    int length = splitValue.length();
+    if (index > 0) {
+      left.value = splitValue.substring(0, index);
+      left.weight = left.value.length();
+    }
+    if (length != index) {
+      right.value = splitValue.substring(index);
+      right.weight = right.value.length();
+    }
+    left.depth = 0;
+    right.depth = 0;
+  }
+
+  // merge two ropes to one parent
+  // return new parent
+  private RopeString concatenate(RopeString left, RopeString right) {
+    if (left == null || right == null) {
+      return left != null ? left : right;
+    }
+    Node rootNode = new Node();
+    rootNode.weight = left.root.weight + right.root.weight;
+    rootNode.left = left.root;
+    rootNode.right = right.root;
+    rootNode.depth = calculateDepth(rootNode) + 1;
+    cleanUp(rootNode);
+    return new RopeString(rootNode);
+  }
+
+  // recursively clean from nulled nodes,
+  // if node have only one child, delete node, and set child to node's parent.
+  private void cleanUp(Node node) {
+    if (node == null || node.isLeaf()) {
+      return;
+    }
+    Node left = node.left;
+    Node right = node.right;
+
+    if (left != null ^ right != null) {
+      Node child = left != null ? left : right;
+      cleanUp(child);
+      node.value = child.value;
+      node.depth = child.depth;
+      node.left = child.left;
+      node.right = child.right;
+    } else {
+      cleanUp(node.left);
+      cleanUp(node.right);
+      node.depth = calculateDepth(node) + 1;
+    }
+
+    if (right != null && right.value == null && right.isLeaf()) {
+      node.right = null;
+    }
+    if (left != null && left.value == null && left.isLeaf()) {
+      node.left = null;
+    }
+  }
+
+  // calculates the depth of the node tree (without node itself)
+  private int calculateDepth(Node node) {
+    Node left = node.left;
+    Node right = node.right;
+    int leftDepth = left != null ? left.depth : 0;
+    int rightDepth = right != null ? right.depth : 0;
+    return Math.max(leftDepth, rightDepth);
+  }
+
+  // recursively populate stringBuilder by nodes values
+  private void createString(Node node, StringBuilder builder) {
+    if (node == null) {
+      return;
+    }
+    if (node.isLeaf() && node.value != null) {
+      builder.append(node.value);
+      return;
+    }
+    createString(node.left, builder);
+    createString(node.right, builder);
+  }
+
+  // collect all leafs to nodeList
+  private void collectAll(Node node, List<Node> nodeList) {
+    if (node == null) {
+      return;
+    }
+    if (node.isLeaf()) {
+      nodeList.add(node);
+      return;
+    }
+    collectAll(node.left, nodeList);
+    collectAll(node.right, nodeList);
+  }
+
+  // combine all nodes to parents
+  private Node combine(List<Node> nodeList, int start, int end) {
+    int range = end - start;
+    switch (range) {
+      case 1:
+        return nodeList.get(start);
+      case 2:
+        return createParentNode(nodeList.get(start), nodeList.get(start + 1));
+      default:
+        int middle = (range / 2) + start;
+        return createParentNode(combine(nodeList, start, middle), combine(nodeList, middle, end));
+    }
+  }
+
+  // create parent node for two orphans
+  private Node createParentNode(Node left, Node right) {
+    Node node = new Node();
+    node.weight = left.weight + right.weight;
+    node.left = left;
+    node.right = right;
+    node.depth = calculateDepth(node) + 1;
+    return node;
+  }
+
+  // node of the Rope tree
+  static class Node {
+
+    Node left;
+    Node right;
+    // String contained in node, if exists
+    String value;
+    // Sum of left nodes lengths
+    int weight;
+    // Length of child chains
+    int depth;
+
+    public Node() {}
+
+    public Node(String value) {
+      this.value = value;
+      this.weight = value.length();
+    }
+
+    public boolean isLeaf() {
+      return left == null && right == null;
+    }
+
+    @Override
+    public String toString() {
+      return value;
+    }
+  }
+
+  // inner class for convenience
+  static class Pair<Rope> {
+    Rope left;
+    Rope right;
+
+    public Pair(Rope left, Rope right) {
+      this.left = left;
+      this.right = right;
+    }
+  }
+}
--- src/test/java/com/williamfiset/algorithms/strings/RopeStringTest.java
+++ src/test/java/com/williamfiset/algorithms/strings/RopeStringTest.java
@@ -1,43 +1,43 @@
-package com.williamfiset.algorithms.datastructures.rope;
+package com.williamfiset.algorithms.strings;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
 
-public class RopeTest {
+public class RopeStringTest {
 
-  private Rope rope;
+  private RopeString rope;
 
   @Before
   public void setUp() {
-    rope = new Rope("da");
+    rope = new RopeString("da");
   }
 
   @Test
   public void ropeCreatingTest() {
     assertThat(rope.toString()).isEqualTo("da");
-    assertThat(rope.getRoot().getWeight()).isEqualTo(2);
+    assertThat(rope.root.weight).isEqualTo(2);
   }
 
   @Test
   public void ropeAppendTest() {
     rope.append("net");
     assertThat(rope.toString()).isEqualTo("danet");
-    assertThat(rope.getRoot().getWeight()).isEqualTo(5);
-    assertThat(rope.getRoot().getLeft().toString()).isEqualTo("da");
-    assertThat(rope.getRoot().getRight().toString()).isEqualTo("net");
+    assertThat(rope.root.weight).isEqualTo(5);
+    assertThat(rope.root.left.toString()).isEqualTo("da");
+    assertThat(rope.root.right.toString()).isEqualTo("net");
   }
 
   @Test
   public void insertTest() {
     rope.insert("sobaka ", 0);
     assertThat(rope.toString()).isEqualTo("sobaka da");
-    rope.insert(" samokat!", rope.getRoot().getWeight());
+    rope.insert(" samokat!", rope.root.weight);
     assertThat(rope.toString()).isEqualTo("sobaka da samokat!");
-    rope.insert(" dama", rope.getRoot().getWeight() / 2);
+    rope.insert(" dama", rope.root.weight / 2);
     assertThat(rope.toString()).isEqualTo("sobaka da dama samokat!");
-    assertThat(rope.getRoot().getWeight()).isEqualTo(23);
+    assertThat(rope.root.weight).isEqualTo(23);
   }
 
   @Test
@@ -59,33 +59,36 @@ public class RopeTest {
     rope.append("beloe");
     rope.append("uho");
     rope.delete(0, 1);
-    rope.insert(" dama", rope.getRoot().getWeight() / 2);
+    rope.insert(" dama", rope.root.weight / 2);
     rope.delete(3, 5);
-    rope.insert(" samokat!", rope.getRoot().getWeight());
+    rope.insert(" samokat!", rope.root.weight);
     rope.insert("sobaka ", 0);
-    assertThat(rope.getRoot().getDepth()).isEqualTo(5);
-    rope.reBalance();
+    assertThat(rope.root.depth).isEqualTo(5);
+    rope.rebalance();
     assertThat(rope.toString()).isEqualTo("sobaka abiamaloeuho samokat!");
-    assertThat(rope.getRoot().getWeight()).isEqualTo(28);
-    assertThat(rope.getRoot().getDepth()).isEqualTo(3);
+    assertThat(rope.root.weight).isEqualTo(28);
+    assertThat(rope.root.depth).isEqualTo(3);
   }
 
   @Test
   public void splitTest() {
     rope.append("net");
-    Rope.Pair<Rope> split = rope.split(2);
-    assertThat(split.getLeft().toString()).isEqualTo("da");
-    assertThat(split.getRight().toString()).isEqualTo("net");
+    rope.delete(1, 2);
+    assertThat(rope.toString()).isEqualTo("det");
+    rope.insert("an", 1);
+    RopeString.Pair<RopeString> split = rope.split(2);
+    assertThat(split.left.toString()).isEqualTo("da");
+    assertThat(split.right.toString()).isEqualTo("net");
     rope.append("bim");
     rope.append("beloe");
     rope.append("uho");
     rope.delete(0, 1);
-    rope.insert(" dama", rope.getRoot().getWeight() / 2);
+    rope.insert(" dama", rope.root.weight / 2);
     rope.delete(3, 5);
-    rope.insert(" samokat!", rope.getRoot().getWeight());
+    rope.insert(" samokat!", rope.root.weight);
     rope.insert("sobaka ", 0);
-    split = rope.split(rope.getRoot().getWeight() / 2);
-    assertThat(split.getLeft().toString()).isEqualTo("sobaka anedamab");
-    assertThat(split.getRight().toString()).isEqualTo("eloeuho samokat!");
+    split = rope.split(rope.root.weight / 2);
+    assertThat(split.left.toString()).isEqualTo("sobaka anedamab");
+    assertThat(split.right.toString()).isEqualTo("eloeuho samokat!");
   }
 }
